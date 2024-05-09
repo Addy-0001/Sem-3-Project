6d7f0286c29281971383b35845dba4ef495d8e74
Adamya <adamsneupane@gmail.com>
2024-05-09 07:23:08 +0545

    initialized backend logic

---
+++ Backend/GymFitProBackend/src/main/java/com/example/gymfitprobackend/GymFitProBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymfitprobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymFitProBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymFitProBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/GymFitProBackend/src/test/java/com/example/gymfitprobackend/GymFitProBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymfitprobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymFitProBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
