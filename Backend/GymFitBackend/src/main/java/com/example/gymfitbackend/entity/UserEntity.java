5b62923a51ebdbba93aa00b8500bab462eb22be2
Adamya <adamsneupane@gmail.com>
2024-05-29 07:36:04 +0545

    updated er diagram

--- Backend/GymFitBackend/src/main/java/com/example/gymfitbackend/entity/UserEntity.java
+++ Backend/GymFitBackend/src/main/java/com/example/gymfitbackend/entity/UserEntity.java
@@ -16,8 +16,8 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name = "username")
-    private String username; // Save the username as is
+    @Column(name = "email_address")
+    private String email_address; // Save the username as is
     @Column(name = "password")
     private String password; // Hash the password and save it (sha256 hash and salt it before storing).
     @Column(name = "is_admin_user")
@@ -25,7 +25,7 @@ public class UserEntity {
     @Column(name = "is_active_user")
     private boolean is_active; // only allow if the user is active
     @Column(name = "payment_due")
-    private boolean payment_due; //tick if the payment is due
+    private boolean payment_due; // tick if the payment is due
     @Column(name = "client_type")
     private String client_type;
 }
