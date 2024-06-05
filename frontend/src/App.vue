e060ca82f9b923be119c39c1ff95c4a86a6170b3
Adamya <adamsneupane@gmail.com>
2024-06-05 08:09:57 +0545

    added frontend files

---
+++ frontend/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav class="bg-black">
+      <div class="nav-left">
+        <router-link to="/" class="logo"> GymFitPro </router-link>
+      </div>
+      <div class="nav-right">
+        <router-link to="/">Dashboard</router-link> |
+        <router-link to="/member-type">Membership Type</router-link> |
+        <router-link to="/attendance">Attendance</router-link> |
+        <router-link to="/exercise">Exercise</router-link> |
+        <router-link to="/members">Members</router-link> |
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+nav a {
+  font-weight: bold;
+  color: #ffffff;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.logo{
+  color: white;
+}
+</style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MembersView from '@/views/MembersView.vue'
+import MemberTypeView from '@/views/MemberTypeView.vue'
+import AttendanceView from '@/views/AttendanceView.vue'
+import ExerciseView from '@/views/ExerciseView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/members',
+    name: 'members',
+    component: MembersView
+  },
+  {
+    path : '/member-type',
+    name : 'member type',
+    component : MemberTypeView
+  },
+  {
+    path : '/attendance',
+    name : 'attendance', 
+    component : AttendanceView
+  },
+  {
+    path : '/exercise',
+    name : 'exercise',
+    component : ExerciseView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
